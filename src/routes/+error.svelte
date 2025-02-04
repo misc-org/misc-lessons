a7f15e52cb202b68c94d661e1c189dab96cb9f4f
shiki <140712093+shiki-01@users.noreply.github.com>
2025-02-04 23:51:45 +0000

    error

---
+++ src/routes/+error.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import 'github-markdown-css/github-markdown-light.css';
+    import { page } from '$app/state';
+</script>
+
+<div class="w-full markdown-body">
+    <h1>Error</h1>
+    <pre>
+        <code>
+            {JSON.stringify($state.snapshot(page.error))}
+        </code>
+    </pre>
+</div>
+
+<style lang="postcss">
+    .markdown-body {
+        background-color: transparent;
+    }
+</style>
