9724cd2ea3d7131ad39229cf8b972b5a4711a5b0
shiki-01 <kishintorii@gmail.com>
2025-02-05 14:56:55 +0900

    save

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -54,7 +54,7 @@
 		</button>
 	</div>
 	<header class="flex h-[60px] w-full items-center justify-center gap-5 bg-[#0e1734]">
-		<a href="/" class="flex h-full w-fit items-center justify-center gap-4">
+		<a href={base + "/"} class="flex h-full w-fit items-center justify-center gap-4">
 			<img {src} alt="svelte" class="h-[20px] object-cover" />
 			<h1 class="text-xl font-bold text-[#fff]">講座総合 MISC Lessons</h1>
 		</a>
